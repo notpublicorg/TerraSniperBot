1c85526c8667e3f999cec29d0d1a569d37dbcda2
Ilshat Ismagilov <ilshat_ismagilov@epam.com>
2021-09-28 13:02:53 +0300

    recursive call test

--- src/terra-processor/repeat.test.ts
+++ src/terra-processor/repeat.test.ts
@@ -1,4 +1,4 @@
-import { firstValueFrom, Observable, repeat, toArray } from 'rxjs';
+import { delay, firstValueFrom, Observable, repeat, tap, toArray } from 'rxjs';
 
 test('repeat', async () => {
   let count = 0;
@@ -8,7 +8,30 @@ test('repeat', async () => {
     subscriber.complete();
   });
 
-  const result = await firstValueFrom(source.pipe(repeat(2), toArray()));
+  const result: string[] = [];
 
-  expect(result).toEqual([0, 1, 2, 3, 4, 5]);
+  await firstValueFrom(
+    source.pipe(
+      tap((v) => result.push(`source ${v}`)),
+      delay(2000),
+      tap((v) => result.push(`after delay ${v}`)),
+      repeat(2),
+      toArray(),
+    ),
+  );
+
+  expect(result).toEqual([
+    'source 0',
+    'source 1',
+    'source 2',
+    'after delay 0',
+    'after delay 1',
+    'after delay 2',
+    'source 3',
+    'source 4',
+    'source 5',
+    'after delay 3',
+    'after delay 4',
+    'after delay 5',
+  ]);
 });
