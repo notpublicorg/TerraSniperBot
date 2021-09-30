e0b0e8bb29230a020ae547b93e4229e08955fe54
Ilshat Ismagilov <ilshat_ismagilov@epam.com>
2021-10-01 01:43:54 +0300

    handling one at a time

---
+++ src/terra-processor/__tests__/delaying.test.ts
@@ -0,0 +1,34 @@
+import { catchError, delay, EMPTY, firstValueFrom, mergeMap, of, tap, toArray } from 'rxjs';
+
+test('delay - handling one at a time', async () => {
+  const log: string[] = [];
+
+  const result = await firstValueFrom(
+    of([1, 2, 3]).pipe(
+      mergeMap(async (values) => {
+        for (const i of values) {
+          try {
+            const obVal = await firstValueFrom(
+              of(i).pipe(
+                tap((v) => log.push(`start handling ${v}`)),
+                mergeMap((v) =>
+                  of(v).pipe(
+                    mergeMap((v) => (v === 1 ? Promise.reject() : Promise.resolve(v))),
+                    delay(3000 - v * 1000),
+                  ),
+                ),
+                tap((v) => log.push(`end handling ${v}`)),
+                catchError(() => EMPTY),
+              ),
+            );
+            return obVal;
+          } catch (e) {}
+        }
+      }),
+      toArray(),
+    ),
+  );
+
+  expect(log).toEqual(['start handling 1', 'start handling 2', 'end handling 2']);
+  expect(result).toEqual([2]);
+});
